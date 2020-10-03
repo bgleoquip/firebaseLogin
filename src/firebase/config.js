2ed1120fc869efe1ebc4656ac6523af799e76a07
bgleoquip <bgleoquip@gmail.com>
2020-10-04 02:07:41 +0530

    stage 8

---
+++ src/firebase/config.js
@@ -0,0 +1,10 @@
+// firebase config
+export const firebaseConfig = {
+    apiKey: "AIzaSyCdw-Ko_nIcjW7_DjvdK2C1U-kxG0bODPg",
+    authDomain: "fir-login-82579.firebaseapp.com",
+    databaseURL: "https://fir-login-82579.firebaseio.com",
+    projectId: "fir-login-82579",
+    storageBucket: "fir-login-82579.appspot.com",
+    messagingSenderId: "939975206218",
+    appId: "1:939975206218:web:22f01ebab34bb0c0ca4db3"
+  };
